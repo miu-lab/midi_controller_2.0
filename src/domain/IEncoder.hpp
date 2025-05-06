75b012e378eaad065d07036230ef96f9b3954761
miu-lab <studio.miu.lab@gmail.com>
2025-05-06 12:04:33 +0200

    Réorganiser les méthodes dans IEncoder pour améliorer la lisibilité et clarifier les méthodes de position absolue

--- src/domain/IEncoder.hpp
+++ src/domain/IEncoder.hpp
@@ -4,8 +4,13 @@
 class IEncoder {
 public:
     virtual ~IEncoder() = default;
-    virtual int8_t    readDelta()       = 0;
-    virtual bool      isPressed() const = 0;
-    virtual EncoderId getId()     const = 0;
-    virtual uint16_t getPpr()     const  = 0;
+    virtual int8_t    readDelta()               = 0;
+    virtual bool      isPressed() const         = 0;
+    virtual EncoderId getId()     const         = 0;
+    virtual uint16_t  getPpr()    const         = 0;
+    
+    // Méthodes pour la position absolue
+    virtual int32_t   getAbsolutePosition() const = 0;
+    virtual int32_t   getPhysicalPosition() const = 0;
+    virtual void      resetPosition()             = 0;
 };
