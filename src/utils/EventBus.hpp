47cd172c8ba435d4d00fa4e22985bc4a4cde0d1f
miu-lab <studio.miu.lab@gmail.com>
2025-05-06 12:02:56 +0200

    Ajouter des messages de débogage pour les fonctions subscribe et publish du bus d'événements

--- src/utils/EventBus.hpp
+++ src/utils/EventBus.hpp
@@ -1,5 +1,7 @@
 #include <vector>
 #include <functional>
+#include <Arduino.h>
+#include "config/debug/DebugConfig.hpp"
 
 template<typename Event>
 class EventBus {
@@ -8,9 +10,21 @@ public:
 
     static void subscribe(Callback cb) {
         getSubscribers().emplace_back(std::move(cb));
+        
+        // Débogage du bus d'événements - fonction subscribe
+#if defined(DEBUG) && defined(DEBUG_EVENT_BUS) && DEBUG_EVENT_BUS
+        Serial.print("EVENT_BUS: Nouvelle souscription - Total: ");
+        Serial.println(getSubscribers().size());
+#endif
     }
 
     static void publish(const Event& event) {
+        // Débogage du bus d'événements - fonction publish
+#if defined(DEBUG) && defined(DEBUG_EVENT_BUS) && DEBUG_EVENT_BUS
+        Serial.print("EVENT_BUS: Publication d'un événement - Destinataires: ");
+        Serial.println(getSubscribers().size());
+#endif
+
         for (const auto& cb : getSubscribers()) {
             cb(event);
         }
