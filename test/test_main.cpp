3643f835514fa7634c8a04621ee403abb8f81a48
miu-lab <studio.miu.lab@gmail.com>
2025-05-08 10:37:43 +0200

    Réorganiser les tests dans test_main.cpp pour regrouper les appels de fonctions de test et améliorer la lisibilité.

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,40 +1,35 @@
 // test/tests_main.cpp
 #include <unity.h>
-
 #include <Arduino.h>
 
-
-
-
-extern void test_read_delta_forward_step();
-extern void test_read_delta_backward_step();
-extern void test_read_multiple_steps();
-extern void test_get_id_and_ppr_and_no_change();
-extern void test_manager_creation();
+// Déclaration des fonctions qui regroupent les tests
+extern void run_quadrature_encoder_tests();
+extern void run_encoder_manager_tests();
 extern void run_event_bus_tests();
 extern void run_input_router_tests();
 extern void run_midi_buffer_tests();
 extern void run_process_buttons_tests();
+extern void run_process_encoders_tests();
 extern void run_profile_manager_tests();
 extern void run_scheduler_tests();
 
-
-
 void setup() {
     UNITY_BEGIN();
-    RUN_TEST(test_read_delta_forward_step);
-    RUN_TEST(test_read_delta_backward_step);
-    RUN_TEST(test_read_multiple_steps);
-    RUN_TEST(test_get_id_and_ppr_and_no_change);
-    RUN_TEST(test_manager_creation);
-
-    run_scheduler_tests();
+    
+    // Exécuter tous les groupes de tests
+    run_quadrature_encoder_tests();
+    run_encoder_manager_tests();
+    run_event_bus_tests();
     run_input_router_tests();
     run_midi_buffer_tests();
     run_process_buttons_tests();
+    run_process_encoders_tests();
     run_profile_manager_tests();
-    run_event_bus_tests();
+    run_scheduler_tests();
+    
     UNITY_END();
 }
 
-void loop() {}
+void loop() {
+    // Vide - requis pour l'architecture Arduino
+}
