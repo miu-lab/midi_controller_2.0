eb57d50f8b6d54784e5a0cfbfacc3c3092da5087
miu-lab <studio.miu.lab@gmail.com>
2025-05-08 21:19:08 +0200

    Améliorer la lisibilité et la structure du code dans BufferedMidiOut en réorganisant les inclusions, en simplifiant les initialisations et en ajoutant des commentaires explicatifs.

--- src/adapters/secondary/midi/BufferedMidiOut.cpp
+++ src/adapters/secondary/midi/BufferedMidiOut.cpp
@@ -1,53 +1,45 @@
 #include "adapters/secondary/midi/BufferedMidiOut.hpp"
 
 BufferedMidiOut::BufferedMidiOut(IMidiOut& output, uint16_t bufferSize)
-    : output_(output)
-    , bufferSize_(bufferSize > 0 ? bufferSize : DEFAULT_BUFFER_SIZE)
-    , nextIndex_(0)
-{
-    // Allouer le buffer
+    : output_(output),
+      bufferSize_(bufferSize > 0 ? bufferSize : DEFAULT_BUFFER_SIZE),
+      nextIndex_(0) {
     buffer_ = new MidiMessage[bufferSize_];
-    
-    // Initialiser tous les messages comme non envoyés
     clear();
 }
 
 void BufferedMidiOut::sendCc(MidiChannel ch, MidiCC cc, uint8_t value) {
-    uint16_t idx = findOrCreateMessage(MessageType::CC, ch, cc);
-    buffer_[idx].value = value;
-    buffer_[idx].sent = false;
+    uint16_t index = findOrCreateMessage(MessageType::CC, ch, cc);
+    buffer_[index].value = value;
+    buffer_[index].sent = false;
 }
 
 void BufferedMidiOut::sendNoteOn(MidiChannel ch, MidiNote note, uint8_t velocity) {
-    uint16_t idx = findOrCreateMessage(MessageType::NOTE_ON, ch, note);
-    buffer_[idx].value = velocity;
-    buffer_[idx].sent = false;
+    uint16_t index = findOrCreateMessage(MessageType::NOTE_ON, ch, note);
+    buffer_[index].value = velocity;
+    buffer_[index].sent = false;
 }
 
 void BufferedMidiOut::sendNoteOff(MidiChannel ch, MidiNote note, uint8_t velocity) {
-    uint16_t idx = findOrCreateMessage(MessageType::NOTE_OFF, ch, note);
-    buffer_[idx].value = velocity;
-    buffer_[idx].sent = false;
+    uint16_t index = findOrCreateMessage(MessageType::NOTE_OFF, ch, note);
+    buffer_[index].value = velocity;
+    buffer_[index].sent = false;
 }
 
 void BufferedMidiOut::flush() {
-    // Envoyer tous les messages qui n'ont pas encore été envoyés
     for (uint16_t i = 0; i < bufferSize_; i++) {
         if (!buffer_[i].sent) {
-            // Envoyer ce message
             switch (buffer_[i].type) {
-                case MessageType::CC:
-                    output_.sendCc(buffer_[i].channel, buffer_[i].control, buffer_[i].value);
-                    break;
-                case MessageType::NOTE_ON:
-                    output_.sendNoteOn(buffer_[i].channel, buffer_[i].control, buffer_[i].value);
-                    break;
-                case MessageType::NOTE_OFF:
-                    output_.sendNoteOff(buffer_[i].channel, buffer_[i].control, buffer_[i].value);
-                    break;
+            case MessageType::CC:
+                output_.sendCc(buffer_[i].channel, buffer_[i].control, buffer_[i].value);
+                break;
+            case MessageType::NOTE_ON:
+                output_.sendNoteOn(buffer_[i].channel, buffer_[i].control, buffer_[i].value);
+                break;
+            case MessageType::NOTE_OFF:
+                output_.sendNoteOff(buffer_[i].channel, buffer_[i].control, buffer_[i].value);
+                break;
             }
-            
-            // Marquer comme envoyé
             buffer_[i].sent = true;
         }
     }
@@ -55,36 +47,44 @@ void BufferedMidiOut::flush() {
 
 void BufferedMidiOut::clear() {
     for (uint16_t i = 0; i < bufferSize_; i++) {
-        buffer_[i].sent = true; // Marquer comme "déjà envoyé" = ne pas envoyer
+        buffer_[i].type = MessageType::CC;
+        buffer_[i].channel = 0;
+        buffer_[i].control = 0;
+        buffer_[i].value = 0;
+        buffer_[i].sent = true;
     }
     nextIndex_ = 0;
 }
 
 uint16_t BufferedMidiOut::findOrCreateMessage(MessageType type, MidiChannel ch, uint8_t control) {
-    // Créer un message temporaire pour la recherche
-    MidiMessage temp = {
-        .type = type,
-        .channel = ch,
-        .control = control,
-        .value = 0,
-        .sent = false
-    };
-    
-    // Chercher un message existant avec le même type, canal et contrôle
+    // Chercher si ce message existe déjà
     for (uint16_t i = 0; i < bufferSize_; i++) {
-        if (buffer_[i] == temp) {
+        if (buffer_[i].type == type && buffer_[i].channel == ch && buffer_[i].control == control) {
             return i;
         }
     }
-    
-    // Pas trouvé, créer un nouveau message
-    if (nextIndex_ < bufferSize_) {
-        uint16_t idx = nextIndex_++;
-        buffer_[idx] = temp;
-        return idx;
+
+    // Chercher un emplacement libre
+    for (uint16_t i = 0; i < bufferSize_; i++) {
+        if (buffer_[i].sent) {
+            buffer_[i].type = type;
+            buffer_[i].channel = ch;
+            buffer_[i].control = control;
+            return i;
+        }
     }
-    
-    // Buffer plein, réutiliser le premier emplacement
-    buffer_[0] = temp;
-    return 0;
+
+    // Si tout est occupé, utiliser l'index suivant (stratégie circulaire)
+    uint16_t index = nextIndex_;
+    nextIndex_ = (nextIndex_ + 1) % bufferSize_;
+
+    buffer_[index].type = type;
+    buffer_[index].channel = ch;
+    buffer_[index].control = control;
+
+    return index;
+}
+
+void BufferedMidiOut::update() {
+    flush();
 }
