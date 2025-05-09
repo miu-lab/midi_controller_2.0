7640d7a5d40fc63348f5537698029fe39d7b91b1
miu-lab <studio.miu.lab@gmail.com>
2025-05-09 22:39:36 +0200

    refactor: Ajouter la configuration et les macros de débogage pour améliorer la traçabilité des événements et des composants

--- src/config/debug/DebugConfig.hpp
+++ src/config/debug/DebugConfig.hpp
@@ -1,6 +1,9 @@
 // config/debug/DebugConfig.hpp
 #pragma once
 
+// Active le débogage global de l'application
+#define DEBUG
+
 // Configuration de débogage
 // 1. Débogage des contrôles physiques de bas niveau (encodeurs, boutons)
 // 0 = Désactivé, 1 = Léger (seulement les mouvements), 2 = Complet (tous les appels)
@@ -8,4 +11,14 @@
 
 // 2. Débogage du bus d'événements (événements publiés/reçus)
 // 0 = Désactivé, 1 = Activé
-#define DEBUG_EVENT_BUS 0
+#define DEBUG_EVENT_BUS_ENABLED 0
+
+// 3. Niveau de débogage global
+// Valeurs possibles : DEBUG_LEVEL_NONE, DEBUG_LEVEL_ERROR, DEBUG_LEVEL_WARNING, 
+//                  DEBUG_LEVEL_INFO, DEBUG_LEVEL_DEBUG, DEBUG_LEVEL_TRACE
+// Par défaut : DEBUG_LEVEL_INFO
+#define DEBUG_LEVEL DEBUG_LEVEL_INFO
+
+// Inclure les macros optimisées
+#include "config/debug/DebugMacros.hpp"
+
---
+++ src/config/debug/DebugMacros.hpp
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <Arduino.h>
+#include "config/debug/DebugConfig.hpp"
+
+// Niveaux de débogage
+#define DEBUG_LEVEL_NONE    0
+#define DEBUG_LEVEL_ERROR   1
+#define DEBUG_LEVEL_WARNING 2
+#define DEBUG_LEVEL_INFO    3
+#define DEBUG_LEVEL_DEBUG   4
+#define DEBUG_LEVEL_TRACE   5
+
+// Niveau de débogage global
+#ifndef DEBUG_LEVEL
+#define DEBUG_LEVEL DEBUG_LEVEL_INFO
+#endif
+
+// Niveaux spécifiques pour les composants
+#define DEBUG_EVENT_BUS_LEVEL DEBUG_LEVEL_INFO
+#define DEBUG_INPUT_LEVEL     DEBUG_LEVEL_INFO
+#define DEBUG_MIDI_LEVEL      DEBUG_LEVEL_INFO
+#define DEBUG_ENCODER_LEVEL   DEBUG_LEVEL_INFO
+#define DEBUG_BUTTONS_LEVEL   DEBUG_LEVEL_INFO
+#define DEBUG_UI_LEVEL        DEBUG_LEVEL_INFO
+
+// Vérifie si Serial est disponible (avec un timeout de 0ms)
+inline bool isSerialReady() {
+    return Serial;
+}
+
+// Macros de débogage optimisées pour la mémoire Flash
+#ifdef DEBUG
+
+// Macro générique avec niveau
+#define DEBUG_LOG(level, format, ...) \
+    do { if (DEBUG_LEVEL >= level && isSerialReady()) { Serial.printf_P(PSTR(format), ##__VA_ARGS__); } } while(0)
+
+// Macros d'erreur (toujours disponibles en mode DEBUG)
+#define DEBUG_ERROR(format, ...) DEBUG_LOG(DEBUG_LEVEL_ERROR, "[ERROR] " format, ##__VA_ARGS__)
+
+// Macros spécifiques aux composants (conditionnelles selon la configuration)
+#if defined(DEBUG_EVENT_BUS_ENABLED) && DEBUG_EVENT_BUS_ENABLED > 0
+    #define DEBUG_EVENT_BUS(format, ...) DEBUG_LOG(DEBUG_EVENT_BUS_LEVEL, "[EB] " format, ##__VA_ARGS__)
+#else
+    #define DEBUG_EVENT_BUS(format, ...) do {} while(0)
+#endif
+
+#if defined(DEBUG_RAW_CONTROLS) && DEBUG_RAW_CONTROLS > 0
+    #define DEBUG_INPUT(format, ...) DEBUG_LOG(DEBUG_INPUT_LEVEL, "[INP] " format, ##__VA_ARGS__)
+    #define DEBUG_ENCODER(format, ...) DEBUG_LOG(DEBUG_ENCODER_LEVEL, "[ENC] " format, ##__VA_ARGS__)
+    #define DEBUG_BUTTON(format, ...) DEBUG_LOG(DEBUG_BUTTONS_LEVEL, "[BTN] " format, ##__VA_ARGS__)
+    
+    // Débogage très détaillé uniquement si niveau > 1
+    #if DEBUG_RAW_CONTROLS > 1
+        #define DEBUG_INPUT_DETAIL(format, ...) DEBUG_LOG(DEBUG_INPUT_LEVEL, "[INP+] " format, ##__VA_ARGS__)
+        #define DEBUG_ENCODER_DETAIL(format, ...) DEBUG_LOG(DEBUG_ENCODER_LEVEL, "[ENC+] " format, ##__VA_ARGS__)
+        #define DEBUG_BUTTON_DETAIL(format, ...) DEBUG_LOG(DEBUG_BUTTONS_LEVEL, "[BTN+] " format, ##__VA_ARGS__)
+    #else
+        #define DEBUG_INPUT_DETAIL(format, ...) do {} while(0)
+        #define DEBUG_ENCODER_DETAIL(format, ...) do {} while(0)
+        #define DEBUG_BUTTON_DETAIL(format, ...) do {} while(0)
+    #endif
+#else
+    #define DEBUG_INPUT(format, ...) do {} while(0)
+    #define DEBUG_ENCODER(format, ...) do {} while(0)
+    #define DEBUG_BUTTON(format, ...) do {} while(0)
+    #define DEBUG_INPUT_DETAIL(format, ...) do {} while(0)
+    #define DEBUG_ENCODER_DETAIL(format, ...) do {} while(0)
+    #define DEBUG_BUTTON_DETAIL(format, ...) do {} while(0)
+#endif
+
+#define DEBUG_MIDI(format, ...) DEBUG_LOG(DEBUG_MIDI_LEVEL, "[MIDI] " format, ##__VA_ARGS__)
+#define DEBUG_UI(format, ...) DEBUG_LOG(DEBUG_UI_LEVEL, "[UI] " format, ##__VA_ARGS__)
+
+// Macros pour stocker les chaînes en mémoire Flash et les utiliser plus efficacement
+#define DEBUG_PRINTLN_FLASH(str) \
+    do { if (isSerialReady()) { Serial.println(F(str)); } } while(0)
+
+#define DEBUG_PRINT_FLASH(str) \
+    do { if (isSerialReady()) { Serial.print(F(str)); } } while(0)
+
+#else // DEBUG n'est pas défini
+// Toutes les macros sont vides si DEBUG n'est pas défini
+#define DEBUG_LOG(level, format, ...) do {} while(0)
+#define DEBUG_ERROR(format, ...) do {} while(0)
+#define DEBUG_EVENT_BUS(format, ...) do {} while(0)
+#define DEBUG_INPUT(format, ...) do {} while(0)
+#define DEBUG_ENCODER(format, ...) do {} while(0)
+#define DEBUG_BUTTON(format, ...) do {} while(0)
+#define DEBUG_INPUT_DETAIL(format, ...) do {} while(0)
+#define DEBUG_ENCODER_DETAIL(format, ...) do {} while(0)
+#define DEBUG_BUTTON_DETAIL(format, ...) do {} while(0)
+#define DEBUG_MIDI(format, ...) do {} while(0)
+#define DEBUG_UI(format, ...) do {} while(0)
+#define DEBUG_PRINTLN_FLASH(str) do {} while(0)
+#define DEBUG_PRINT_FLASH(str) do {} while(0)
+#endif
