f65f6593d3931b27e02e7dda650180152083c5d9
miu-lab <studio.miu.lab@gmail.com>
2025-05-10 09:05:33 +0200

    feat: Améliorer la configuration de débogage avec des niveaux définis et des macros optimisées

--- src/config/debug/DebugConfig.hpp
+++ src/config/debug/DebugConfig.hpp
@@ -1,28 +1,85 @@
 // config/debug/DebugConfig.hpp
 #pragma once
 
-// Active le débogage global de l'application
-#define DEBUG
+//---------------------------------------------------------------------------
+// Définitions des niveaux de débogage (ne pas modifier)
+//---------------------------------------------------------------------------
+#define DEBUG_LEVEL_NONE 0     // Aucun message de débogage
+#define DEBUG_LEVEL_ERROR 1    // Erreurs seulement
+#define DEBUG_LEVEL_WARNING 2  // Erreurs + avertissements
+#define DEBUG_LEVEL_INFO 3     // Niveau informatif général
+#define DEBUG_LEVEL_DEBUG 4    // Informations détaillées pour le développement
+#define DEBUG_LEVEL_TRACE 5    // Suivi d'exécution très détaillé
 
-// Configuration de débogage
-// 1. Débogage des contrôles physiques de bas niveau (encodeurs, boutons)
-// 0 = Désactivé, 1 = Léger (seulement les mouvements), 2 = Complet (tous les appels)
+//---------------------------------------------------------------------------
+// Configuration des environnements de débogage (via platformio.ini)
+//---------------------------------------------------------------------------
+
+// PRODUCTION: Débogage minimal (défini dans platformio.ini comme CONFIG_PRODUCTION)
+#if defined(CONFIG_PRODUCTION)
+#undef DEBUG  // Assurer que DEBUG n'est pas défini en production
+#define DEBUG_LEVEL DEBUG_LEVEL_ERROR
 #define DEBUG_RAW_CONTROLS 0
-
-// 2. Débogage du bus d'événements (événements publiés/reçus)
-// 0 = Désactivé, 1 = Activé
 #define DEBUG_EVENT_BUS_ENABLED 0
+#define DEBUG_TASK_SCHEDULER_LEVEL 0  // Désactiver toutes les statistiques
+#define DEBUG_SCHEDULER_LEVEL DEBUG_LEVEL_NONE  // Définir à NONE en production
 
-// 3. Débogage de l'ordonnanceur de tâches (statistiques de tâches)
-// 0 = Désactivé, 1 = Activé
-#define DEBUG_TASK_SCHEDULER 0
-
-// 4. Niveau de débogage global
-// Valeurs possibles : DEBUG_LEVEL_NONE, DEBUG_LEVEL_ERROR, DEBUG_LEVEL_WARNING, 
-//                  DEBUG_LEVEL_INFO, DEBUG_LEVEL_DEBUG, DEBUG_LEVEL_TRACE
-// Par défaut : DEBUG_LEVEL_INFO
+// DEVELOPMENT: Débogage standard (défini dans platformio.ini comme CONFIG_DEVELOPMENT)
+#elif defined(CONFIG_DEVELOPMENT)
+#ifndef DEBUG
+#define DEBUG  // Activer DEBUG s'il n'est pas déjà défini
+#endif
 #define DEBUG_LEVEL DEBUG_LEVEL_INFO
+#define DEBUG_RAW_CONTROLS 1  // Débogage léger des contrôles
+#define DEBUG_EVENT_BUS_ENABLED 1
+#define DEBUG_TASK_SCHEDULER_LEVEL 1  // Stats CPU basiques seulement
+#define DEBUG_SCHEDULER_LEVEL DEBUG_LEVEL_INFO  // Définir le niveau pour l'ordonnanceur
 
-// Inclure les macros optimisées
-#include "config/debug/DebugMacros.hpp"
+// VERBOSE: Débogage maximal (défini dans platformio.ini comme CONFIG_VERBOSE)
+#elif defined(CONFIG_VERBOSE)
+#ifndef DEBUG
+#define DEBUG  // Activer DEBUG s'il n'est pas déjà défini
+#endif
+#define DEBUG_LEVEL DEBUG_LEVEL_DEBUG
+#define DEBUG_RAW_CONTROLS 2  // Débogage complet des contrôles
+#define DEBUG_EVENT_BUS_ENABLED 1
+#ifndef DEBUG_TASK_SCHEDULER_LEVEL
+#define DEBUG_TASK_SCHEDULER_LEVEL 2  // Stats CPU complètes (basiques + détaillées)
+#endif
+#define DEBUG_SCHEDULER_LEVEL DEBUG_LEVEL_DEBUG  // Définir le niveau pour l'ordonnanceur
 
+// Configuration par défaut (si aucun environnement n'est spécifié)
+#else
+// Activer DEBUG uniquement s'il n'est pas déjà défini ou explicitement désactivé
+#if !defined(DEBUG) && !defined(NDEBUG)
+#define DEBUG
+#endif
+
+// Définir les valeurs par défaut seulement si elles ne sont pas déjà définies
+#ifndef DEBUG_LEVEL
+#define DEBUG_LEVEL DEBUG_LEVEL_INFO
+#endif
+
+#ifndef DEBUG_RAW_CONTROLS
+#define DEBUG_RAW_CONTROLS 0
+#endif
+
+#ifndef DEBUG_EVENT_BUS_ENABLED
+#define DEBUG_EVENT_BUS_ENABLED 0
+#endif
+
+// Niveau de logs pour l'ordonnanceur (0=désactivé, 1=stats CPU basiques, 2=stats détaillées)
+#ifndef DEBUG_TASK_SCHEDULER_LEVEL
+#define DEBUG_TASK_SCHEDULER_LEVEL 1
+#endif
+
+// Niveau pour les messages du scheduler
+#ifndef DEBUG_SCHEDULER_LEVEL
+#define DEBUG_SCHEDULER_LEVEL DEBUG_LEVEL_INFO
+#endif
+#endif  // Ferme le bloc #if/#elif/#else
+
+//---------------------------------------------------------------------------
+// Inclusion des macros de débogage
+//---------------------------------------------------------------------------
+// Les macros de débogage sont dans DebugMacros.hpp qui inclut ce fichier
--- src/config/debug/DebugMacros.hpp
+++ src/config/debug/DebugMacros.hpp
@@ -2,19 +2,9 @@
 
 #include <Arduino.h>
 #include "config/debug/DebugConfig.hpp"
+#include "config/debug/SerialBuffer.hpp"
 
-// Niveaux de débogage
-#define DEBUG_LEVEL_NONE    0
-#define DEBUG_LEVEL_ERROR   1
-#define DEBUG_LEVEL_WARNING 2
-#define DEBUG_LEVEL_INFO    3
-#define DEBUG_LEVEL_DEBUG   4
-#define DEBUG_LEVEL_TRACE   5
-
-// Niveau de débogage global
-#ifndef DEBUG_LEVEL
-#define DEBUG_LEVEL DEBUG_LEVEL_INFO
-#endif
+// Note: Les niveaux de débogage sont maintenant définis dans DebugConfig.hpp
 
 // Niveaux spécifiques pour les composants
 #define DEBUG_EVENT_BUS_LEVEL DEBUG_LEVEL_INFO
@@ -23,28 +13,52 @@
 #define DEBUG_ENCODER_LEVEL   DEBUG_LEVEL_INFO
 #define DEBUG_BUTTONS_LEVEL   DEBUG_LEVEL_INFO
 #define DEBUG_UI_LEVEL        DEBUG_LEVEL_INFO
+#ifndef DEBUG_SCHEDULER_LEVEL  // Ne pas redéfinir si déjà défini
 #define DEBUG_SCHEDULER_LEVEL DEBUG_LEVEL_INFO
+#endif
 
 // Vérifie si Serial est disponible (avec un timeout de 0ms)
 inline bool isSerialReady() {
     return Serial;
 }
 
+// Fonction pour ajouter un saut de ligne et limiter la longueur des messages
+inline void formatDebugMessage(const String& msg) {
+    // Limite la longueur de la ligne à 80 caractères maximum
+    String formattedMsg;
+    if (msg.length() > 80) {
+        formattedMsg = msg.substring(0, 77) + "...";
+    } else {
+        formattedMsg = msg;
+    }
+    
+    // Utilise le tampon série si disponible, sinon affiche directement
+    SerialBuffer::println(formattedMsg);
+}
+
 // Macros de débogage optimisées pour la mémoire Flash
 #ifdef DEBUG
 
-// Macro générique avec niveau
+// Macro générique avec niveau et ajout de saut de ligne
 #define DEBUG_LOG(level, format, ...) \
-    do { if (DEBUG_LEVEL >= level && isSerialReady()) { Serial.printf_P(PSTR(format), ##__VA_ARGS__); } } while(0)
+    do { if (DEBUG_LEVEL >= level && isSerialReady()) { \
+        char buffer[100]; \
+        snprintf_P(buffer, sizeof(buffer), PSTR(format), ##__VA_ARGS__); \
+        formatDebugMessage(String(buffer)); \
+    }} while(0)
 
 // Macros d'erreur (toujours disponibles en mode DEBUG)
 #define DEBUG_ERROR(format, ...) DEBUG_LOG(DEBUG_LEVEL_ERROR, "[ERROR] " format, ##__VA_ARGS__)
 
 // Macros spécifiques aux composants (conditionnelles selon la configuration)
 #if defined(DEBUG_EVENT_BUS_ENABLED) && DEBUG_EVENT_BUS_ENABLED > 0
+#ifndef DEBUG_EVENT_BUS  // Ne pas redéfinir si déjà défini ailleurs
     #define DEBUG_EVENT_BUS(format, ...) DEBUG_LOG(DEBUG_EVENT_BUS_LEVEL, "[EB] " format, ##__VA_ARGS__)
+#endif
 #else
+#ifndef DEBUG_EVENT_BUS  // Ne pas redéfinir si déjà défini ailleurs
     #define DEBUG_EVENT_BUS(format, ...) do {} while(0)
+#endif
 #endif
 
 #if defined(DEBUG_RAW_CONTROLS) && DEBUG_RAW_CONTROLS > 0
@@ -75,15 +89,26 @@ inline bool isSerialReady() {
 #define DEBUG_UI(format, ...) DEBUG_LOG(DEBUG_UI_LEVEL, "[UI] " format, ##__VA_ARGS__)
 
 // Débogage pour l'ordonnanceur de tâches
-#if defined(DEBUG_TASK_SCHEDULER) && DEBUG_TASK_SCHEDULER > 0
+// Niveau 0 : Désactivé
+// Niveau 1 : Stats basiques
+// Niveau 2 : Stats détaillées
+#if defined(DEBUG_TASK_SCHEDULER_LEVEL) && DEBUG_TASK_SCHEDULER_LEVEL >= 1
     #define DEBUG_SCHEDULER(format, ...) DEBUG_LOG(DEBUG_SCHEDULER_LEVEL, "[SCHED] " format, ##__VA_ARGS__)
+    
+    // Débogage détaillé pour l'ordonnanceur de tâches (statistiques détaillées)
+    #if DEBUG_TASK_SCHEDULER_LEVEL >= 2
+        #define DEBUG_SCHEDULER_VERBOSE(format, ...) DEBUG_LOG(DEBUG_SCHEDULER_LEVEL, "[SCHED-DETAIL] " format, ##__VA_ARGS__)
+    #else
+        #define DEBUG_SCHEDULER_VERBOSE(format, ...) do {} while(0)
+    #endif
 #else
     #define DEBUG_SCHEDULER(format, ...) do {} while(0)
+    #define DEBUG_SCHEDULER_VERBOSE(format, ...) do {} while(0)
 #endif
 
 // Macros pour stocker les chaînes en mémoire Flash et les utiliser plus efficacement
 #define DEBUG_PRINTLN_FLASH(str) \
-    do { if (isSerialReady()) { Serial.println(F(str)); } } while(0)
+    do { if (isSerialReady()) { SerialBuffer::println(F(str)); } } while(0)
 
 #define DEBUG_PRINT_FLASH(str) \
     do { if (isSerialReady()) { Serial.print(F(str)); } } while(0)
@@ -92,7 +117,9 @@ inline bool isSerialReady() {
 // Toutes les macros sont vides si DEBUG n'est pas défini
 #define DEBUG_LOG(level, format, ...) do {} while(0)
 #define DEBUG_ERROR(format, ...) do {} while(0)
+#ifndef DEBUG_EVENT_BUS
 #define DEBUG_EVENT_BUS(format, ...) do {} while(0)
+#endif
 #define DEBUG_INPUT(format, ...) do {} while(0)
 #define DEBUG_ENCODER(format, ...) do {} while(0)
 #define DEBUG_BUTTON(format, ...) do {} while(0)
@@ -101,6 +128,8 @@ inline bool isSerialReady() {
 #define DEBUG_BUTTON_DETAIL(format, ...) do {} while(0)
 #define DEBUG_MIDI(format, ...) do {} while(0)
 #define DEBUG_UI(format, ...) do {} while(0)
+#define DEBUG_SCHEDULER(format, ...) do {} while(0)
+#define DEBUG_SCHEDULER_VERBOSE(format, ...) do {} while(0)
 #define DEBUG_PRINTLN_FLASH(str) do {} while(0)
 #define DEBUG_PRINT_FLASH(str) do {} while(0)
 #endif
